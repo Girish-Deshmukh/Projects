d2afd7f7d0cff590e20ed08a7ee2c4a7db887706
Girish Deshmukh <47716181+Girish-Deshmukh@users.noreply.github.com>
2022-09-09 13:09:57 +0530

    Create FunctionsDemo.py

---
+++ Python/PythonBasics/FunctionsDemo.py
@@ -0,0 +1,22 @@
+# In Python, function is a group of related statements that perform a specific task.
+
+def greetme(name):
+    print("Good Morning " + name)
+
+
+def AddIntegers(a, b):
+    print(a + b)
+
+
+greetme("Girish Deshmukh")
+AddIntegers(2, 3)
+
+# ----------------- Example of return -------------------#
+print(" **************************************************** ")
+
+
+def AddIntegers(c, d):
+    return c + d
+
+
+print(AddIntegers(5, 2))
